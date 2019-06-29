013525e9e4118a9fb348a4dc244764178da70e65
Freitas29 <arao.freitas@hotmail.com>
2019-06-29 11:50:32 -0300

    request to games

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react'
+import api from '../../services/api'
 
 export default class Main extends Component{
+
+  async componentDidMount() {
+    let response = await api.get('/v1/games')
+    console.log('response', response)
+  }
+
   render(){
     return(
       <div className="Main">
